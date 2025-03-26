3f20eb12f8e89ac1bfb9586466e7fdcb96160d7a
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2025-03-26 16:27:37 -0300

    wip: adicionando as rotas

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,7 @@
+
+import 'package:agrosense_app/app/ui/screens/home/home.dart';
+import 'package:agrosense_app/app/ui/screens/sign_in/sign_in.dart';
+import 'package:agrosense_app/app/ui/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatefulWidget {
@@ -11,9 +15,12 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp( 
-      initialRoute: "",
+      debugShowCheckedModeBanner: false,
+      theme: AgroSenseTheme.theme,
+      initialRoute: "/sign_in",
       routes: {
-        
+        "/home" : (context) => Home(),
+        "/sign_in": (context) => SignIn()
       },
     );
   }

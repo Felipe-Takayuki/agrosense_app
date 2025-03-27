2173f7301f3b76b3519a885fab67f073742b42d9
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2025-03-27 16:48:39 -0300

    adicionando  configurações

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:agrosense_app/app/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "assets/.env");
+  await Supabase.initialize(anonKey: dotenv.env["SUPABASE_API_KEY"]!, url: dotenv.env["SUPABASE_URL"]!);
   runApp(const App());
 }

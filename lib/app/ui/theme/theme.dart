92f2b792407dcda93fb936acc69b74c2709f4a3f
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2025-04-03 10:10:09 -0300

    começando o tema dark

--- lib/app/ui/theme/theme.dart
+++ lib/app/ui/theme/theme.dart
@@ -23,15 +23,42 @@ class AgroSenseTheme{
     textSelectionTheme: TextSelectionThemeData(
       cursorColor: Colors.black, 
     ),
-
     elevatedButtonTheme: ElevatedButtonThemeData(
 
       style: ElevatedButton.styleFrom(
-        backgroundColor: Color(0xffB79408), 
+        backgroundColor: Color.fromARGB(255, 255, 205, 7), 
+        textStyle: TextStyle(color: Colors.white),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10)))
+      ),
+    ),
+    
+  );
+
+  static final darkTheme = ThemeData(
+    scaffoldBackgroundColor: const Color.fromARGB(255, 54, 54, 54),
+    textTheme: TextTheme(bodySmall: TextStyle(color: Colors.white)),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Color.fromARGB(255, 255, 205, 7), 
         textStyle: TextStyle(color: Colors.white),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10)))
       )
+    ),
+    
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: Color.fromARGB(255, 255, 255, 255),
+      disabledBorder: _inputBorder,
+      border: _inputBorder,
+      enabledBorder: _inputBorder,
+      labelStyle: TextStyle(color: const Color.fromARGB(255, 0, 0, 0)),
+      errorBorder: _inputBorder,
+      focusedBorder: _inputBorder,
+      focusColor: Colors.grey,
       
-    )
+    ),
+     
+    textSelectionTheme: TextSelectionThemeData(selectionColor: Colors.white)
   );
 }

37961efe0a60edf40df85f46ef0188beb0078722
Felipe-Takayuki <felipe.takahashi@fatec.sp.gov.br>
2025-04-29 10:11:34 -0300

    refact: verifição de nulo por operador

--- lib/app/service/camera_service.dart
+++ lib/app/service/camera_service.dart
@@ -1,26 +1,23 @@
-
 import 'package:shared_preferences/shared_preferences.dart';
 
 class CameraService {
-   
   Future<SharedPreferences> _instance() async {
-    SharedPreferences sharedPreferences =await SharedPreferences.getInstance();
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     return sharedPreferences;
-  } 
+  }
 
+  Future<void> _addImage(String newImage) async {
+    final instance = await _instance();
+    List<String> listaAtual = instance.getStringList('images') ?? [];
 
-Future<void> _addImage(String newImage) async {
-  final instance = await _instance();
-  List<String> listaAtual = instance.getStringList('images') ?? [];
+    listaAtual.add(newImage);
 
-  listaAtual.add(newImage);
-
-  await instance.setStringList('images', listaAtual);
-}
+    await instance.setStringList('images', listaAtual);
+  }
 
   void saveImage(String imagePath) async {
-    final instancia =await _instance();
-    instancia.setString("image",imagePath);   
+    final instancia = await _instance();
+    instancia.setString("image", imagePath);
     _addImage(imagePath);
   }
 
@@ -31,26 +28,23 @@ Future<void> _addImage(String newImage) async {
     listaAtual.add(newImage);
 
     await instance.setStringList('gallery_images', listaAtual);
-  } 
+  }
 
   Future<String> getImage() async {
     final instance = await _instance();
-    final path = instance.getString("image");
-    return path!;
+    final path = instance.getString("image") ?? "";
+    return path;
   }
-  
+
   Future<List<String?>> getImages() async {
     final instancia = await _instance();
-    final listas = instancia.getStringList("images");
-    return listas!;
+    final listas = instancia.getStringList("images") ?? [];
+    return listas;
   }
-  
+
   Future<List<String?>> getSavedImages() async {
     final instancia = await _instance();
     final listas = instancia.getStringList("gallery_images") ?? [];
     return listas;
   }
-  
-
-  
 }

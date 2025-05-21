af74330069b6c58f35140bd15e8ea073559035e8
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2025-05-21 18:31:19 -0300

    formatando a data

--- lib/app/ui/widgets/home_widgets/weather_component.dart
+++ lib/app/ui/widgets/home_widgets/weather_component.dart
@@ -1,7 +1,7 @@
 import 'package:agrosense_app/app/ui/screens/home/home_controller.dart';
 import 'package:agrosense_app/app/ui/widgets/home_widgets/weather_info.dart';
 import 'package:flutter/material.dart';
-
+import 'package:intl/intl.dart';
 class WeatherComponent extends StatefulWidget {
   final HomeController controller;
   final Size size;
@@ -19,18 +19,19 @@ class _WeatherComponentState extends State<WeatherComponent> {
     return Container(
       width: widget.size.width * 0.88,
       // height removido para ajustar automaticamente
-      padding: const EdgeInsets.all(20),
+      padding: const EdgeInsets.all(20), 
       decoration: BoxDecoration(
         color: const Color.fromARGB(255, 255, 205, 7),
         borderRadius: BorderRadius.circular(16),
       ),
-      child: Column(
+      child: Column(        
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
             widget.controller.selectedTrap!.name,
             style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
+          Text(DateFormat("dd/MM/yyyy").format(widget.controller.selectedTrap!.date)),
           const SizedBox(height: 8),
           WeatherInfo(
             label: "Umidade Relativa",
@@ -45,11 +46,11 @@ class _WeatherComponentState extends State<WeatherComponent> {
             icon: Icons.thermostat,
             color: Colors.red,
           ),
-          const Divider(color: Colors.red, height: 20,),
+          const Divider(color: Colors.red),
           WeatherInfo(
             label: "Direção do Vento",
             info: widget.controller.selectedTrap!.weatherModel.toWindDirectionString(),
-            icon: Icons.wind_power_outlined,
+            icon: Icons.wind_power_outlined, 
             color: Colors.green,
           ),
           const Divider(color: Colors.green),
